fe73d439d7debb823d7a96c5bf07f7ea54c51072
Noach Meged <noachmeged@MacBook-Pro-van-Noach.local>
2023-12-02 16:30:36 +0100

    done

---
+++ Lab | SQL Intro.sql
@@ -0,0 +1,16 @@
+
+-- Get all the data from tables actor, film and customer.
+select * from film;
+select * from actor;
+select * from customer;
+-- Get film titles 
+SELECT title from sakila.film;
+-- Get unique list of film languages under the alias language.
+select count(language_ID) from language;
+-- 5.1 Find out how many stores does the company have
+select count(store_ID) as number_of_stores from sakila.store;
+-- 5.2 Find out how many employees staff does the company have?
+select count(staff_id) as number_of_staff from sakila.staff;
+-- 5.3 Return a list of employee first names only?
+select first_name as names from sakila.staff;
+
