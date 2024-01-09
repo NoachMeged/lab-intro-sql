3b459eb21598c846555eaed90d60a831eb9c0bc1
Noach Meged <noachmeged@MacBook-Pro-van-Noach.local>
2024-01-09 20:57:43 +0100

    done

--- Lab | SQL Intro.sql
+++ Lab | SQL Intro.sql
@@ -6,7 +6,7 @@ select * from customer;
 -- Get film titles 
 SELECT title from sakila.film;
 -- Get unique list of film languages under the alias language.
-select count(language_ID) from language;
+select language_ID as language from language;
 -- 5.1 Find out how many stores does the company have
 select count(store_ID) as number_of_stores from sakila.store;
 -- 5.2 Find out how many employees staff does the company have?
